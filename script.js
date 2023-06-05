6cb7baf46259ce2f878d8d16d4f0595d8ceda213
psantoso13 <prayogo.santoso@gmail.com>
2023-06-06 05:44:48 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+let homeScore = document.getElementById('home')
+let awayScore = document.getElementById('away')
+
+let countHome = 0;
+let countAway = 0;
+
+function homeOne() {
+    countHome += 1;
+    homeScore.textContent = countHome;
+}
+
+function homeTwo() {
+    countHome += 2;
+    homeScore.textContent = countHome;
+}
+
+function homeThree() {
+    countHome += 3;
+    homeScore.textContent = countHome;
+}
+
+function awayOne() {
+    countAway += 1;
+    awayScore.textContent = countAway;
+}
+
+function awayTwo() {
+    countAway += 2;
+    awayScore.textContent = countAway;
+}
+
+function awayThree() {
+    countAway += 3;
+    awayScore.textContent = countAway;
+}
+
+function resetScore() {
+    homeScore.textContent = 0
+    awayScore.textContent = 0
+    countHome = 0
+    countAway = 0
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        cairo: "'Cairo', sans-serif",
+        chakra: "'Chakra Petch', sans-serif",
+        fraunces: "'Fraunces', serif",
+      }
+    },
+  },
+  plugins: [],
+}
+
